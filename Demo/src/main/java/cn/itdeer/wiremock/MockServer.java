1b6f5a71c169bad0422e067bee6ca2576965a0bd
itdeer.cn <itdeer.cn@gmail.com>
2019-04-22 17:44:26 +0800

    edit

--- Demo/src/main/java/cn/itdeer/wiremock/MockServer.java
+++ Demo/src/main/java/cn/itdeer/wiremock/MockServer.java
@@ -24,21 +24,31 @@ public class MockServer {
 
         mock("/order/1","data/01.json");
 
-        mock("/v2/movie/in_theaters_all","data/inTheaters_all.json");
         mock("/v2/movie/in_theaters_list","data/inTheaters_list.json");
+        mock("/v2/movie/in_theaters_all","data/inTheaters_all.json");
+        mock("/v2/movie/in_theaters_20","data/inTheaters_20.json");
+        mock("/v2/movie/in_theaters_40","data/inTheaters_40.json");
 
-        mock("/v2/movie/coming_soon_all","data/comingSoon_all.json");
         mock("/v2/movie/coming_soon_list","data/comingSoon_list.json");
+        mock("/v2/movie/coming_soon_all","data/comingSoon_all.json");
+        mock("/v2/movie/coming_soon_20","data/comingSoon_20.json");
+        mock("/v2/movie/coming_soon_40","data/comingSoon_40.json");
+        mock("/v2/movie/coming_soon_60","data/comingSoon_60.json");
+        mock("/v2/movie/coming_soon_80","data/comingSoon_80.json");
 
-        mock("/v2/movie/top250_all","data/top250_all.json");
         mock("/v2/movie/top250_list","data/top250_list.json");
+        mock("/v2/movie/top250_all","data/top250_all.json");
+        mock("/v2/movie/top250_20","data/top250_20.json");
+        mock("/v2/movie/top250_40","data/top250_40.json");
+        mock("/v2/movie/top250_60","data/top250_60.json");
+        mock("/v2/movie/top250_80","data/top250_80.json");
 
     }
 
     public static void mock(String url,String name) throws IOException{
         ClassPathResource resource = new ClassPathResource(name);
 //        String content = FileUtils.readFileToString(resource.getFile());
-        String content = StringUtils.join(FileUtils.readLines(resource.getFile(),"UTF-8"),"\n");
+        String content = StringUtils.join(FileUtils.readLines(resource.getFile(),"UTF-8"),"\r\n");
         WireMock.stubFor(WireMock.get(WireMock.urlPathEqualTo(url)).willReturn(WireMock.aResponse().withBody(content).withStatus(200)));
     }
 
